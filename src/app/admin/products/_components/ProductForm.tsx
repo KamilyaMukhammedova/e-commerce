44c02d1b8d3f7288954e68e08d7c87fb64c4ebf4
Kamilya <kamilya@Kamilyas-MacBook-Pro.local>
2024-08-07 22:49:27 +1000

    Added edit product page

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -7,11 +7,15 @@ import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { formatCurrency } from '@/lib/formatters';
 import { Button } from '@/components/ui/button';
-import { addProduct } from '@/app/admin/_actions/products';
+import { addProduct, updateProduct } from '@/app/admin/_actions/products';
+import { Product } from '@prisma/client';
+import Image from 'next/image';
 
-export function ProductForm() {
-    const [error, action] = useFormState(addProduct, {});
-    const [priceInCents, setPriceInCents] = useState<number>();
+export function ProductForm({product} : {product? : Product | null}) {
+    const [error, action] =
+        useFormState(!product ? addProduct : updateProduct.bind(null, product.id), {});
+
+    const [priceInCents, setPriceInCents] = useState<number | undefined>(product?.priceInCents);
 
     return (
         <form
@@ -20,7 +24,7 @@ export function ProductForm() {
         >
             <div className={'space-y-2'}>
                 <Label htmlFor={'name'}>Name</Label>
-                <Input type={'text'} id={'name'} name={'name'} required={true}/>
+                <Input type={'text'} id={'name'} name={'name'} required={true} defaultValue={product?.name || ''}/>
                 {error.name && <div className={'text-destructive'}>{error.name}</div>}
             </div>
             <div className={'space-y-2'}>
@@ -38,17 +42,19 @@ export function ProductForm() {
             </div>
             <div className={'space-y-2'}>
                 <Label htmlFor={'description'}>Description</Label>
-                <Textarea id={'description'} name={'description'} required={true}/>
+                <Textarea id={'description'} name={'description'} required={true} defaultValue={product?.description || ''}/>
                 {error.description && <div className={'text-destructive'}>{error.description}</div>}
             </div>
             <div className={'space-y-2'}>
                 <Label htmlFor={'file'}>File</Label>
-                <Input type={'file'} id={'file'} name={'file'} required={true}/>
+                <Input type={'file'} id={'file'} name={'file'} required={!product}/>
+                {product && <div className={'text-muted-foreground'}>{product.filePath}</div>}
                 {error.file && <div className={'text-destructive'}>{error.file}</div>}
             </div>
             <div className={'space-y-2'}>
                 <Label htmlFor={'image'}>Image</Label>
-                <Input type={'file'} id={'image'} name={'image'} required={true}/>
+                <Input type={'file'} id={'image'} name={'image'} required={!product}/>
+                {product && <Image src={product.imagePath} alt={'Product image'} width={'400'} height={'400'}/>}
                 {error.image && <div className={'text-destructive'}>{error.image}</div>}
             </div>
             <SubmitButton/>
